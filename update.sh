5b8c2196d9e788a113fa6ad76418fa9818ffdb06
Sidney Trzepacz <sidtrzisyourleader@gmail.com>
2022-06-01 16:59:21 -0400

    Updated the updater (ironic, isn't it)

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt install git -y
 git fetch origin --recurse-submodules=no --progress --prune
 npm install
-sudo apt update
-sudo apt upgrade -y
+pkexec apt update && apt upgrade -y
